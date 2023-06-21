4248a55488eff19976572e176ff55d0b345a82e9
IrynaS1 <snowqueen-2007@mail.ru>
2023-06-21 13:23:19 +0300

    modified blog.js - refactoring

--- script/module/blog.js
+++ script/module/blog.js
@@ -1,4 +1,5 @@
 const arrayPages = [100, 8, 10, 15, 25];
+console.log('arrayPages', arrayPages);
 
 const firstPage = arrayPages[0];
 
@@ -10,24 +11,16 @@ const goods = async (page) => {
 	return data.data;
 };
 
-const renderGoods = async (page) => {
-	console.log('page in renderGoods', page);
-	const blog = document.querySelector('.blog');
+const createPage = (element, blog) => {
+	const article = document.createElement('article');
 
-	const data = await goods(page);
+	const articleLink = document.createElement('a');
+	articleLink.setAttribute('href', `http://127.0.0.1:5500/blogs/article.html?id=${element.id}`);
 
-	console.log('data', data);
+	const card = document.createElement('div');
+	card.classList.add('blog-card');
 
-	data.forEach(element => {
-		const article = document.createElement('article');
-
-		const articleLink = document.createElement('a');
-		articleLink.setAttribute('href', `http://127.0.0.1:5500/blogs/article.html?id=${element.id}`);
-
-		const card = document.createElement('div');
-		card.classList.add('blog-card');
-
-		card.innerHTML = `<div class="blog-card">
+	card.innerHTML = `<div class="blog-card">
 			<div class="blog-card__image"></div>
 			<div class="blog-card__description">
 				<div class="blog-card__title">
@@ -49,54 +42,36 @@ const renderGoods = async (page) => {
 			</div>
 		</div>`;
 
-		articleLink.appendChild(card);
-		article.appendChild(articleLink);
+	articleLink.appendChild(card);
+	article.appendChild(articleLink);
 
-		blog.appendChild(article);
-	});
+	blog.appendChild(article);
 };
-/* 
-const createNewPage = (page) => {
-	const actualUrl = window.location.href;
 
-	const subString = '?page=';
+const renderGoods = async (page, blog) => {
+	console.log('page in renderGoods', page);
 
-	if (subsring) {
-		const indexSubString = actualUrl.indexOf(subString);
+	const data = await goods(page);
 
-		const url = actualUrl.substring(0, indexSubString);
+	console.log('data', data);
 
-		if (page === 1) {
-			const createPage = url;
+	data.forEach(element => {
+		createPage(element, blog);
+	});
 
-			return createPage;
-		} else {
-			const createPage = url + `?page=${page}`;
-
-			return createPage;
-		}
-	} else {
-		return actualUrl;
-	}
-}; */
+	return blog;
+};
 
 const createPaginationBlock = () => {
-	const url = window.location.href;
 	const paginationBlock = document.querySelector('.pagination__pages-numbers');
 
 	for (let i = 0; i <= arrayPages.length - 1; i++) {
-		const paginationLink = document.createElement('a');
-		paginationLink.setAttribute('href', `${url}`);
-		paginationLink.classList.add('pagination__link');
-
 		const paginationPageNumber = document.createElement('p');
 		paginationPageNumber.classList.add('pagination__pages-numb');
 
 		paginationPageNumber.innerText = `${i}`;
 
-		paginationLink.appendChild(paginationPageNumber);
-
-		paginationBlock.appendChild(paginationLink);
+		paginationBlock.appendChild(paginationPageNumber);
 	}
 
 	return paginationBlock;
@@ -108,37 +83,68 @@ const pagination = () => {
 	const paginationNumbers = document.querySelectorAll('.pagination__pages-numb');
 
 	const paginationLinks = document.querySelectorAll('.pagination__link');
+	paginationNumbers[0].innerHTML = '1';
 
-	for (let i = 0; i <= paginationNumbers.length - 1; i++) {
+	for (let i = 1; i <= paginationNumbers.length - 1; i++) {
 		paginationNumbers[i].innerHTML = `${i + 1}`;
-
-		paginationLinks[i].href = `http://127.0.0.1:5500/blogs/blog.html?page=${i + 1}`;
 	}
 };
 
 const start = (page) => {
-	renderGoods(page);
+	const blog = document.querySelector('.blog');
+	console.log('blog in start', blog);
+
+	renderGoods(page, blog);
+
+	console.log('blog in start -------', blog);
 
 	pagination();
 
 	const startPagination = document.querySelector('.pagination__pages-numb');
-	startPagination.classList.add('active_page-block');
+	startPagination.classList.add('active_page-block', 'active_page-content');
 };
 
 start(firstPage);
 
+const newPageUrl = (page) => {
+	if (page === '1') {
+		window.location.assign(
+			`${window.location.origin}${window.location.pathname}`);
+	} else {
+		const params = new URLSearchParams();
+
+		params.set('page', `${page}`);
+		console.log(params.toString());
+
+		window.location.assign(
+			`${window.location.origin}${window.location.pathname}?${params.toString()}`);
+	}
+};
+
 const pages = document.querySelector('.pagination__pages-numbers');
-console.log('pages', pages);
 
 pages.addEventListener('click', (e) => {
 	e.preventDefault;
 
+	const blog = document.querySelector('.blog');
+	blog.innerHTML = '';
+
+	const oldPageNumber = document.querySelector('.active_page-block');
+	oldPageNumber.classList.remove('active_page-block', 'active_page-content');
+
+	const ev = e.target;
+	ev.classList.add('active_page-block', 'active_page-content');
+
+	console.log(' e.target.', e.target);
+
 	const clickNumber = e.target.innerHTML;
 
+	newPageUrl(clickNumber);
+
 	const indexClickNumber = clickNumber - 1;
-	console.log('indexClickNumber', indexClickNumber);
 
 	const arrayListIndex = arrayPages[indexClickNumber];
 
-	renderGoods(arrayListIndex);
+	renderGoods(arrayListIndex, blog);
 });
+
